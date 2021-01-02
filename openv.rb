d24fc1658ab979a2e298aef82d024a1bc18bd02f
Kohei Morita <mrtc0@ssrf.in>
2021-01-02 17:42:30 +0900

    bump up

--- openv.rb
+++ openv.rb
@@ -1,8 +1,8 @@
 class Openv < Formula
   desc "A tool that uses the credentials stored in 1password as an environment variable."
   homepage "https://github.com/mrtc0/openv"
-  url "https://github.com/mrtc0/openv/releases/download/v0.1.0/openv-x86_64-apple-darwin.zip"
-  sha256 "98c32c1525201509148b5d9a3ad9efefb137ced84ecc639402582829505070e0"
+  url "https://github.com/mrtc0/openv/releases/download/v0.1.1/openv-x86_64-apple-darwin.zip"
+  sha256 "c7cd168649755d1a52176a971ce224df680dca068b15aad8441fe1ac4633a3d8"
   license "Apache-2.0"
 
   def install
