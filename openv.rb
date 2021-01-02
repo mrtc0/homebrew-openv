0b0d179b35ed4dfe019d0b6194097b1f39b99125
Kohei Morita <mrtc0@ssrf.in>
2021-01-02 17:57:18 +0900

    fix shasum

--- openv.rb
+++ openv.rb
@@ -1,9 +1,9 @@
 class Openv < Formula
-  desc "A tool that uses the credentials stored in 1password as an environment variable."
-  homepage "https://github.com/mrtc0/openv"
-  url "https://github.com/mrtc0/openv/releases/download/v0.1.1/openv-x86_64-apple-darwin.zip"
-  sha256 "c7cd168649755d1a52176a971ce224df680dca068b15aad8441fe1ac4633a3d8"
-  license "Apache-2.0"
+  desc 'A tool that uses the credentials stored in 1password as an environment variable.'
+  homepage 'https://github.com/mrtc0/openv'
+  url 'https://github.com/mrtc0/openv/releases/download/v0.1.1/openv-x86_64-apple-darwin.zip'
+  sha256 '7e11d049065b72aa12fd3ca071f3bb5ade5c5ccde03014677786a778c8402564'
+  license 'Apache-2.0'
 
   def install
     bin.install Dir['openv']
